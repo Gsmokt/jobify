f9f55292b4a06d79342f03c60c3484c2ae0f0bf0
Grzegorz Smoktunowicz <biarth@wp.pl>
2022-07-18 12:23:07 +0200

    build version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Error from "./pages/Error";
+import Register from "./pages/Register";
+import ProtectedRoute from "./pages/ProtectedRoute";
+import AllJobs from "./pages/dashboard/AllJobs";
+import Profile from "./pages/dashboard/Profile";
+import SharedLayout from "./pages/dashboard/SharedLayout";
+import Stats from "./pages/dashboard/Stats";
+import AddJob from "./pages/dashboard/AddJob";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Route index element={<Stats />} />
+          <Route path="all-jobs" element={<AllJobs />} />
+          <Route path="add-job" element={<AddJob />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+        <Route path="/register" element={<Register />} />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
